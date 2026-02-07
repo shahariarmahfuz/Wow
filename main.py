97bae4d155796cc1c07b3e8c0d90ca933937d50a
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2026-02-07 17:55:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.config["SECRET_KEY"] = "super_secret_key_change_this"
 # --- DATABASE CONFIGURATION (PostgreSQL) ---
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://postgres:DeTxIpImDQDTixhRgIRlsXdTYkokvGtd@centerbeam.proxy.rlwy.net:42228/railway"
+] = "postgresql://postgres:EsckSLtXBklbTzeuVqLeTqDQQWNtbmxl@shortline.proxy.rlwy.net:18866/railway"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # --- REMEMBER ME CONFIGURATION (30 Days) ---
